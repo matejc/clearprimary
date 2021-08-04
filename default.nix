1aad8829f892fa363e302ea95c18095cfb747529
Matej Cotman <matej.cotman@eficode.com>
2021-08-05 01:49:23 +0300

    chore: remove rec

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with pkgs;
 let
   buildCmd = "gcc -o clearprimary $(pkg-config --cflags --libs gtk+-3.0 glib-2.0 gdk-3.0 atk) clearprimary.c";
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   name = "clearprimary";
   src = ./.;
   buildInputs = [ coreutils gcc glib.dev gtk3.dev atk.dev pkgconfig ];
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   '';
   installPhase = ''
     mkdir -p $out/bin
-    cp ${name} $out/bin/
+    cp clearprimary $out/bin/
   '';
   shellHook = ''
     ${bear}/bin/bear -- ${buildCmd}
